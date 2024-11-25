f51593eaed913ae2e88f849103207874912d7927
XiomaraHuamani <xiomara.huamani.c@gmail.com>
2024-11-25 18:22:51 -0500

    uso de uuid

--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -6,11 +6,11 @@ class Blog(models.Model):
     id = models.AutoField(primary_key=True)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     uniqueId = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    titulo = models.CharField(max_length=255, help_text="Título del blog")
+    titulo = models.TextField(help_text="Título del blog")  # Cambiado a TextField
     descripcion = models.TextField(help_text="Descripción breve del blog")
     contenido = models.TextField(help_text="Contenido completo del blog")
-    autor = models.CharField(max_length=100, help_text="Nombre del autor")
-    categoria_nombre = models.CharField(max_length=100, help_text="Categoría asociada al blog")
+    autor = models.TextField(help_text="Nombre del autor")  # Cambiado a TextField
+    categoria_nombre = models.TextField(help_text="Categoría asociada al blog")  # Cambiado a TextField
     img1 = models.ImageField(upload_to='blog_images/', blank=True, null=True)
     img2 = models.ImageField(upload_to='blog_images/', blank=True, null=True)
     img3 = models.ImageField(upload_to='blog_images/', blank=True, null=True)
